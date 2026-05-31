a58f03be43cd3c38c3a9517d20e94ff416aa94b3
mostwise <shxrk@shxrk.dev>
2026-05-31 11:33:01 -0400

    feat(main): remove UI, keep application a simple cli tool

--- src/ui/electron/preload.cjs
+++
@@ -1,9 +0,0 @@
-const { contextBridge } = require("electron");
-
-contextBridge.exposeInMainWorld(
-	"winstroDesktop",
-	Object.freeze({
-		isDesktop: true,
-		company: "Wicked Softworks",
-	}),
-);
